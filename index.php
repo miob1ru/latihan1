5f63e08877a0a31f85ee88dd2361ba7d12bbcfb3
unknown <putrasumawe56@gmail.com>
2020-03-21 12:54:49 +0700

    Operator dalam PHP

--- index.php
+++ index.php
@@ -1,40 +1,7 @@
 <?php
-//operator perbandingan
-	var_dump(1 < 5) ;
-		echo "</br>";
 
-	var_dump(1 > 5) ;
-		echo "</br>";
+	$nama_depan = "Andik";
+	$nama_belakang = "Surya";
 
-	var_dump(1 <= 5) ;
-		echo "</br>";
-
-	var_dump(1 =< 5) ;
-		echo "</br>";
-
-	var_dump(1 == 5) ;
-		echo "</br>";
-
-	var_dump(1 != 5) ;
-		echo "</br>";
-
-	var_dump(1 === "1") ;
-		echo "</br>";
-
-	var_dump(1 !== "1") ;
-		echo "</br>";
-
-//operator identitas
-	var_dump(1 === "1") ;
-		echo "</br>";
-
-	var_dump(1 !== "1") ;
-		echo "</br>";
-
-//operator logika
-	$x = 10;
-	var_dump($x < 20 && $x % 2 == 0) ;
-		echo "</br>";
-	var_dump($x < 20 || $x % 2 == 0) ;
-		echo "</br>";
+	echo $nama_depan . "" . $nama_belakang;
 ?>
