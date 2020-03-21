4e99f190e87991be46038a17bdcdc75350a8a72d
unknown <putrasumawe56@gmail.com>
2020-03-21 12:07:00 +0700

    Konsep Interpolasi

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
+$nama = "Andik";
 
+echo "Hallo, Selamat Datang $nama </br>";
+echo 'Hallo, Selamat Datang $nama';
 
-// variabel dan type data
-// variable = digunakan untuk menyimpan data
-// aturan variabel 'tidak boleh diawali dengan angka, tapi boleh mengandung angka'
-
-$nama = "Andi";
 ?>
-
-<!DOCTYPE html>
-
-<html>
-<head>
-	<title>
-		Belajar PHP
-	</title>
-</head>
-<body>
-	<h1>Hallo , Selamat Datang <?php echo $nama; ?></h1>
-</body>
-</html>
