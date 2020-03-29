6151a1848700e63e0df5964db6dce69990a3d48a
unknown <putrasumawe56@gmail.com>
2020-03-29 11:06:47 +0700

    Task 1

--- minggu4/latihan.php
+++ minggu4/latihan.php
@@ -1,11 +1,11 @@
 <?php;
-//pengulangan
-//for
+// pengulangan
+// for
 
 for($i = 0; $i < 5; i++){
 	echo "Hello Word! <br>";
 }
-/* $i = 0; bagian inisialisasi/menentukan variable
-$1 < 5; bagian kondisi / untuk memberhentikan kondisi perulangan ketika berjumlah <5
-i++ bagian incerment (menambah) kondisi */
+/* $i = 0; 		bagian inisialisasi/ menentukan variable
+$1 < 5; 		bagian kondisi / untuk memberhentikan kondisi perulangan ketika berjumlah <5
+$i++ 			bagian incerment (menambah) kondisi */
 ?>
