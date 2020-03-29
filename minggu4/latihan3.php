4938adb8c4e3a3ae010f54ae479161696b837a92
unknown <putrasumawe56@gmail.com>
2020-03-29 15:37:27 +0700

    Kondisi 2

--- minggu4/latihan3.php
+++ minggu4/latihan3.php
@@ -1,7 +1,9 @@
 <?php
-$x = 10;	#kondisi variabel
-if ($x < 20){#kondisi pengecekan dalam bracket
+$x = 20;	#kondisi variabel
+if ($x < 10){#kondisi pengecekan dalam bracket
 	echo "benar"; #jika TRUE ini yang dicetak
+	} elseif ($x == 20){ #kondisi tepat
+		echo "BINGGO!";
 	} else {
 		echo "salah"; #jika FALSE ini yang dicetak
 	}
