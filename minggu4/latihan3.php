10f8151467670a60d45a32b626d03919241ba90d
unknown <putrasumawe56@gmail.com>
2020-03-29 15:33:21 +0700

    Task 8

---
+++ minggu4/latihan3.php
@@ -0,0 +1,8 @@
+<?php
+$x = 10;	#kondisi variabel
+if ($x < 20){#kondisi pengecekan dalam bracket
+	echo "benar"; #jika TRUE ini yang dicetak
+	} else {
+		echo "salah"; #jika FALSE ini yang dicetak
+	}
+?>
