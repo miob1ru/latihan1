015cb5a70a0556b1585bcfcd60f1d918d492241e
unknown <putrasumawe56@gmail.com>
2020-03-29 15:55:46 +0700

    Tabel Praktikum

--- minggu4/latihan2.php
+++ minggu4/latihan2.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang = "en">
 <head>
-		<meta charst="UTF-8">
-		<tittle>Latihan Looping</tittle>
+    <meta charset = "UTF - 8">
+    <title>Latihan Looping</title>
 </head>
 <body>
-	<h2>Membuat Perulangan Tabel</h2>
-	<table border="1" cellpadding="10" cellspacing="0">
-		<?php
-		for ($b = 1; $b <= 8; $b++) {# $b = untuk baris
-			echo "<tr>";
-			for ($k = 1; $k <= 10; $k++) {#$k =untuk kolom
-				echo "<td> $b,$k </td>";
-			}
-			echo "</tr>";
-		}
-		?>
-	</table>
+    <h2>Membuat perulangan tabel</h2>
+    <table border = "1" cellpadding = "10" cellspacing = "0">
+
+        <?php
+            for ( $b = 1; $b <= 10; $b++ ) { # $b = untuk baris
+
+                    if ($b % 2 == 0){ // kondisi
+                        echo "<tr>";    // konidisi true
+                    }
+                    else {
+                        echo "<tr bgcolor = #F08080>"; // konddisi false
+                    }
+
+                for ( $k = 1; $k <= 8; $k++ ) { # $k = untuk kolom
+                    echo "<td> $b , $k </td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+
+    </table>
 </body>
 </html>
