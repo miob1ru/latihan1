f5bbbeaadc932d3362bdda42acc33db8d5c4b83c
unknown <putrasumawe56@gmail.com>
2020-04-16 22:01:08 +0700

    Task 3

--- minggu7/array2.php
+++ minggu7/array2.php
@@ -1,5 +1,11 @@
 <?php 
-$mahasiswa = ["Andi Surya","19510014","Sistem Informasi","putrasumawe56@gmail.com"];
+//array multidimensi/array didalam array bersarang
+$mahasiswa = [
+	["Andi Surya","19510014","Sistem Informasi","putrasumawe56@gmail.com"],
+	["Andi","19510014","Sistem Informasi","sumawe56@gmail.com"],
+	["Permana","19510014","Sistem Informasi","Putra56@gmail.com"]
+
+];
 ?>
 <!DOCTYYPE html>
 <html lang="en">
@@ -10,12 +16,12 @@ $mahasiswa = ["Andi Surya","19510014","Sistem Informasi","putrasumawe56@gmail.co
 </head>
 	<body>
 	<h1>Daftar Mahasiswa</h1>
+<? php foreach ($mahasiswa as $mhs): ?>
 	<ul>
-		<?php foreach ($mahasiswa as $mhs): ?>
-		<li>
-			<?php echo $mhs; ?>
-		</li>
+		<?php foreach ($mhs as $m): ?>
+			<li> <?php echo $m; ?> </li>
 		<?php endforeach; ?>
 	</ul>
+<?php endforeach; ?>
 	</body>
 </html>
